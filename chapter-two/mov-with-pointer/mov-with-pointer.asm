3114d8970422138587462296a1bf1d575f1e918f
Magno Jovino <magno.jovino@hotmail.com>
2025-02-23 16:58:56 -0300

    feat: add mov with pointer code

---
+++ chapter-two/mov-with-pointer/mov-with-pointer.asm
@@ -0,0 +1,12 @@
+global _start
+
+section .data
+test: dq -1
+
+section .text
+
+_start:
+    mov byte[test], 1   ;1
+    mov word[test], 1   ;2
+    mov dword[test], 1  ;3
+    mov qword[test], 1  ;4
